4571a5a15d94b18d36206571849f4958256a4fe2
ravenusmc <mcuddy77@gmail.com>
2018-12-24 09:56:59 -0500

    able to add books to project

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # configuration
@@ -11,13 +11,46 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
-    test = 'Hello Mike!!!'
-    test2 = 'MMM'
-    return jsonify(test, test2)
+    test = "PONG!!!"
+    return jsonify(test)
+
+#Books route
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
